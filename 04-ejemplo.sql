95960dd10c4dcd4013a9a6a748f019ade29c51c0
Breyson Olivera <breyolic@gmail.com>
2018-03-05 10:33:18 -0500

    Se agregó sentencias para modificar la hora en el servidor

--- 04-ejemplo.sql
+++ 04-ejemplo.sql
@@ -7,3 +7,7 @@ SELECT month(curdate()), monthname(now());
 SELECT day(now()), dayname(now()), dayofmonth(now()), dayofyear(now());
 
 SELECT @@global.time_zone, @@session.time_zone;
+
+SET time_zone = '-05:00';
+
+SET global time_zone = '-05:00';
